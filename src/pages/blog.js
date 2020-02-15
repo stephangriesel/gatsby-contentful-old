98c97149651f1fa8bb83e8664f305d842b7d286e
Stephan <sgriesel@gmail.com>
2020-02-15 08:58:10 +0100

    add link component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from 'gatsby'
 
 export default class blog extends Component {
     render() {
         return (
             <div>
                 blog
+                <div>
+                    {/* <a href="/blog">Blog</a> */}
+                    <Link to="/">Home</Link>
+                </div>
             </div>
         )
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div>
+        <h1>Home</h1>
+        Hello world!
+        <div>
+            {/* <a href="/blog">Blog</a> */}
+            <Link to="/blog">Blog</Link>
+        </div>
+        <div>
+            <a href="https://www.gatsbyjs.org">Gatsby Docs</a>
+        </div>
+    </div>
+)
