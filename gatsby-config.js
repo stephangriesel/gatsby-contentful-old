19382423bc648c9f932d4b38b178e7b3d682c9d9
Stephan <sgriesel@gmail.com>
2020-02-17 06:55:16 +0100

    meta data change

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Stephan\'s Site',
+    description: 'Practice practice practice',
+    author: '@stevegriesel',
+    data: {
+      nickname: 'stevie',
+      age: 34
+    },
+  },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }
