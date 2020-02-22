2d3b1574f797392bf6986d1bc18b6f27f30bc75e
Stephan <sgriesel@gmail.com>
2020-02-22 15:36:24 +0100

    add examples

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -2,19 +2,44 @@ import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import styled from 'styled-components'
 import img from '../img/img3.jpg'
-import Image from 'gatsby-image'
+import Image from 'gatsby-image' // Gatsby Image Component
+
+const getImg = graphql`
+{
+    fixed: file(relativePath: {eq: "img1.jpg"}) {
+      childImageSharp {
+        fixed(width: 200, grayscale:true ) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    fluid: file(relativePath: {eq: "img2.jpg"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+  
+`
 
 const Images = () => {
+    const data = useStaticQuery(getImg);
+    // console.log(data);
     return (
         <Wrapper>
             <article>
                 <h3>basic img</h3>
+                <img src={img} className="basic" />
             </article>
             <article>
                 <h3>fixed img/blur</h3>
+                <Image fixed={data.fixed.childImageSharp.fixed} />
             </article>
             <article>
                 <h3>fluid img/svg</h3>
+                <Image fluid={data.fluid.childImageSharp.fluid} />
             </article>
         </Wrapper>
     )
@@ -25,6 +50,9 @@ text-align:center;
 text-transform:capitalize;
 width:80vw;
 margin:0 auto 10rem auto;
+.basic {
+    width:100%;
+}
 article {
     border:1px solid #000;
     padding:1rem 0;
