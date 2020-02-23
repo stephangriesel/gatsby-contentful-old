0ecfda458e1bc610e0f4c98a59db3c9fe23cf2b2
Stephan <sgriesel@gmail.com>
2020-02-23 14:53:37 +0100

    add styling & render data

--- src/components/Tours/Tours.js
+++ src/components/Tours/Tours.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import SingleTour from './SingleTour'
+import styles from '../../css/items.module.css'
 
 const Tours = ({ tours }) => {
     console.log(tours)
     return (
-        <div>
-            tours component
-            <SingleTour />
-        </div>
+        <section className={styles.tours}>
+            <h2>Tours</h2>
+            <div className={styles.center}>
+                {tours.map(({ node }) => {
+                    return <SingleTour key={node.contentful_id} tour={node} />
+                })}
+            </div>
+        </section>
     )
 }
 
