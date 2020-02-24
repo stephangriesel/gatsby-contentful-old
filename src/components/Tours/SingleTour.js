a91918db10f69e5f9d0e78192ba3f0529c318236
Stephan <sgriesel@gmail.com>
2020-02-24 06:24:10 +0100

    add react icons, make it dynamic

--- src/components/Tours/SingleTour.js
+++ src/components/Tours/SingleTour.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import { FaMap } from 'react-icons/fa'
+import Image from 'gatsby-image'
+import styles from '../../css/tour.module.css'
 
-const SingleTour = () => {
+const SingleTour = ({ tour }) => {
+    // console.log(tour)
+    const { name, price, country, days, slug, images } = tour
+    const mainImage = images[0].fluid;
     return (
-        <div>
-            single tour
-        </div>
+        <article className={styles.tour}>
+            <div className={styles.imgContainer}>
+                <Image fluid={mainImage} className={styles.img} alt={name} />
+                <Link className={styles.link} to={`/tours/${slug}`}>More</Link>
+            </div>
+            <div className={styles.footer}>
+                <h3>{name}</h3>
+                <div className={styles.info}>
+                    <h4 className={styles.country}>
+                        <FaMap className={styles.icon} />
+                        {country}
+                    </h4>
+                </div>
+                <div className={styles.details}>
+                    <h6>{days} days</h6>
+                    <h6>from  ${price}</h6>
+                </div>
+            </div>
+        </article>
     )
 }
 
