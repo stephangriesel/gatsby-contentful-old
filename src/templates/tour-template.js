04c1e2abfaf076c785ef43b0ec95c2f3d53f77e5
Stephan <sgriesel@gmail.com>
2020-02-29 12:25:19 +0100

    update tour template & add query

--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import styles from '../css/template.module.css'
+import Image from 'gatsby-image'
+import { FaMoneyBillWave, FaMap } from 'react-icons/fa'
+import { Link } from 'gatsby'
+
 
 const TourTemplate = props => {
     console.log(props)
@@ -9,4 +16,28 @@ const TourTemplate = props => {
     )
 }
 
+
+export const query = graphql`
+query($slug:String!) {
+    tour:contentfulTourExample(slug:{eq:$slug}){
+      name
+      price
+      country
+      days
+      start(formatString:"dddd - Do MMMM, YYYY")
+      journey {
+        day
+        info
+      }
+      description{
+        description
+      }
+      images {
+        fluid {
+          srcSet
+        }
+      }
+    }
+  }
+`
 export default TourTemplate
