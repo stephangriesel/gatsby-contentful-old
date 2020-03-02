2c6ddcb8090c421e929387ad4c9de0e669a473f3
Stephan <sgriesel@gmail.com>
2020-03-02 06:22:53 +0100

    update styles using modules

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
-// import styles from '../sass/navbar.module.scss'
+import styles from '../css/navbar.module.css'
 
 const Navbar = () => {
     return (
         <nav>
-            <ul>
+            <ul className={styles.menu}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/blog/">Blog</Link></li>
-                <li><Link to="/products/">Products</Link></li>
-                <li><Link to="/examples/">Examples</Link></li>
-                <li><Link to="/images/">Images</Link></li>
+                {/* <li><Link to="/products/">Products</Link></li> */}
+                {/* <li><Link to="/examples/">Examples</Link></li> */}
+                {/* <li><Link to="/images/">Images</Link></li> */}
                 <li><Link to="/tours/">Tours</Link></li>
             </ul>
         </nav>
