df2e9157a6f3be3b555c6b1bbb17b472d13796da
Stephan <sgriesel@gmail.com>
2020-03-15 07:42:48 +0100

    destructure & update to dynamic fields

--- src/components/Updates/SingleUpdate.js
+++ src/components/Updates/SingleUpdate.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Image from 'gatsby-image'
+import { Link } from 'gatsby'
 
-const SingleUpdate = () => {
+const SingleUpdate = ({ update }) => {
+    console.log(update);
+    const { updatesTitle, updatesDate, updatesSlug, images, updatesDescription: { updatesDescription } } = update
+    const updateImage = images[0].fluid
     return (
         <div>
-            Single Update Component
+            <div>
+                <Image fluid={updateImage} alt={updatesTitle} />
+            </div>
+            <div>
+                <h3>{updatesTitle}</h3>
+                <h6>{updatesDate}</h6>
+                <p>{updatesDescription}</p>
+                <Link to={`/${updatesSlug}`}>.. more</Link>
+            </div>
         </div>
     )
 }
