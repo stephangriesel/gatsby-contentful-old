3caddbbbd33ce59ad225f7a8e98154bd6e7989f7
Stephan <sgriesel@gmail.com>
2020-03-15 11:40:30 +0100

    change navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,22 +4,24 @@ import styles from '../css/navbar.module.css'
 
 const Navbar = () => {
     return (
-        <div>
-            {/* <div>
-                <h1>Stephan's Tours</h1>
-            </div> */}
-            <nav>
-                <ul className={styles.menu}>
-                    <li><Link to="/">Home</Link></li>
-                    {/* <li><Link to="/blog/">Blog</Link></li> */}
-                    {/* <li><Link to="/products/">Products</Link></li> */}
-                    {/* <li><Link to="/examples/">Examples</Link></li> */}
-                    {/* <li><Link to="/images/">Images</Link></li> */}
-                    <li><Link to="/tours/">Tours</Link></li>
-                    <li><Link to="/contact/">Contact</Link></li>
-                </ul>
-            </nav>
-        </div>
+        <header>
+            <div className={styles.logo}>
+                test
+            </div>
+            <div className={styles.menuWrapper}>
+                <nav>
+                    <ul className={styles.menu}>
+                        <li><Link to="/">Home</Link></li>
+                        {/* <li><Link to="/blog/">Blog</Link></li> */}
+                        {/* <li><Link to="/products/">Products</Link></li> */}
+                        {/* <li><Link to="/examples/">Examples</Link></li> */}
+                        {/* <li><Link to="/images/">Images</Link></li> */}
+                        <li><Link to="/tours/">Tours</Link></li>
+                        <li><Link to="/contact/">Contact</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
